98d8cb5cb41ae37057de9a0dcf7d251a345633b6
guillenfernanda45-droid <guillenfernanda45@gmail.com>
2026-01-28 00:44:10 +0000

    app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("Calculadora de Propinas")
+st.sidebar.header("Registre los datos de la cuenta")
+Total = st.sidebar.number_input("Monto total de la cuenta", min_value=0.0, step=0.01)
+Porcentaje = st.sidebar.number_input("Porcentaje de la propina (%)", min_value=0, step=1)
+if st.sidebar.button("Calcular Propina"): 
+    Propina = Total * (Porcentaje / 100)
+    st.metric(label="Propina sugerida", value=f"${Propina:,.2f}")
+    
